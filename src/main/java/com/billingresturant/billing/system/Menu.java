e6f0069c752497209d9ed98a299e4b2fa43ab39f
=coder4938942308 <billyr264@outlook.com>
2022-10-27 13:35:09 +0100

    Third commit

--- src/main/java/com/billingresturant/billing/system/Menu.java
+++ src/main/java/com/billingresturant/billing/system/Menu.java
@@ -1,22 +1,28 @@
 package com.billingresturant.billing.system;
 
 public class Menu{
+    private int number;
     private String main;
     private double price;
 
-    public Menu(String main, double price){
+    public Menu(int number , String main, double price){
         this.main = main;
         this.price = price;
+        this.number = number;
     }
     public Menu(Menu source) {
         this.main = source.main;
         this.price = source.price;
+        this.number = source.number;
     }
     public String getMain(){
         return this.main;
     }
     public double getPrice(){
         return this.price;
+    }
+    public int getNumber(){
+        return this.number;
     }
     public void setMenu(String main) {
         this.main = main;
--- src/main/java/com/billingresturant/billing/system/ResturantBillingSystemApplication.java
+++ src/main/java/com/billingresturant/billing/system/ResturantBillingSystemApplication.java
@@ -1,8 +1,9 @@
 package com.billingresturant.billing.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Array;
 import java.util.Arrays;
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -10,19 +11,25 @@ public class ResturantBillingSystemApplication {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		Menu[] menu = new Menu[]{
-				new Menu("Pizza", 10.99),
-				new Menu("Lasagna", 9.99),
-				new Menu("Chilli Con Cani", 9.99),
-				new Menu("Stir Fry" , 8.99)
+				new Menu(1, "Pizza", 10.99),
+				new Menu(2, "Lasagna", 9.99),
+				new Menu(3, "Chilli Con Cani", 9.99),
+				new Menu(4, "Stir Fry" , 8.99)
 		};
 		System.out.println("How Many People?");
 		System.out.println("Enter How Many:   ");
 		int customers = scan.nextInt();
-		for (int i = 0; i <= customers; i++){
+		for (int i = 0; i <= customers -1; i++) {
 			System.out.println("Person:  " + i);
+			for (int j = 0; j < menu.length; j++) {
+				System.out.println(menu[j]);
+
 			}
+			System.out.println("What Would You Like");
 
+			int mealNumber = scan.nextInt();
 
+		}
 
 		}
 	}
